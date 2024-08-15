93dd78940a71ecdcd2daee55e94a89c12020e1b6
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-08-15 18:50:48 +0530

    updated path

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const fs = require('fs');
-const path = require('path');
+const dbPath = "./db.txt";
+
 const getTodosSync = () => {
-  const read = fs.readFileSync("./db.txt","utf-8");
-  return read
+  const read = fs.readFileSync(dbPath, "utf-8");
+  return read;
 };
 
 const getTodoSync = (id) => {
-  const data = fs.readFileSync("./db.txt","utf-8");
+  const data = fs.readFileSync(dbPath, "utf-8");
   const formattedData = "[" + data.split("}\n{").join("},{") + "]";
   const todos = JSON.parse(formattedData);
-  const todo = todos.find((item )=> item.id == id);
+  const todo = todos.find((item) => item.id == id);
   return JSON.stringify(todo);
 };
 
 const createTodoSync = (todo) => {
   const obj = {
-    id : Date.now(),
-    title : todo,
-    isCompleted : false,
-    createdAt : new Date().toISOString(),
-    updatedAt : new Date().toISOString(),
-  }
-  const str = JSON.stringify(obj,null,2);
-  fs.appendFileSync("./db.txt", str + "\n");
+    id: Date.now(),
+    title: todo,
+    isCompleted: false,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  };
+  const str = JSON.stringify(obj, null, 2);
+  fs.appendFileSync(dbPath, str + "\n");
 };
 
 const updateTodoSync = (id, updates) => {
-  const todos = fs.readFileSync(dbPath, 'utf-8').split('\n');
+  const todos = fs.readFileSync(dbPath, 'utf-8').split('\n').filter(Boolean);
   const updatedTodos = todos.map((t) => {
-      const todo = JSON.parse(t);
-      if (todo.id === id) {
-          return JSON.stringify({
-              ...todo,
-              ...updates,
-              updatedAt: new Date().toISOString(),
-          }, null, 2);
-      }
-      return t;
+    const todo = JSON.parse(t);
+    if (todo.id === id) {
+      return JSON.stringify({
+        ...todo,
+        ...updates,
+        updatedAt: new Date().toISOString(),
+      }, null, 2);
+    }
+    return t;
   });
   fs.writeFileSync(dbPath, updatedTodos.join('\n') + '\n');
 };
-
 
 const deleteTodoSync = (id) => {
   const todos = fs.readFileSync(dbPath, 'utf-8').split('\n').filter(Boolean);
@@ -55,3 +55,4 @@ module.exports = {
   updateTodoSync,
   deleteTodoSync,
 };
+
